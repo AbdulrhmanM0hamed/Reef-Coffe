378f09b46ba614d1a50f70a17ad4d98fd5b2e14d
Abdulrhman Mohammed Shabaan <126196441+AbdulrhmanM0hamed@users.noreply.github.com>
2025-01-24 21:37:13 +0200

    fix issue of google

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
@@ -15,31 +16,23 @@ import 'package:hyper_market/feature/profile/presentation/cubit/theme_cubit.dart
 import 'package:hyper_market/feature/splash/view/splash_view.dart';
 import 'package:hyper_market/generated/l10n.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
+
 
 void main() async {
+ 
+
   try {
     WidgetsFlutterBinding.ensureInitialized();
-
-    dotenv.env['SUPABASE_URL'] = 'https://kizgmgaocdhnarvqtzvf.supabase.co';
-    dotenv.env['SUPABASE_ANON_KEY'] =
-        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtpemdtZ2FvY2RobmFydnF0enZmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzMzMjQ5NjksImV4cCI6MjA0ODkwMDk2OX0.LwosgMdM5ZcZAeVxn3b84lIeO4K6_-l4BsYF5pxxkJg';
 
     await Prefs.init();
     await Prefs.clearInvalidData();
 
     setupServiceLocator();
 
-    final supabaseUrl = dotenv.env['SUPABASE_URL'];
-    final supabaseKey = dotenv.env['SUPABASE_ANON_KEY'];
-
-    if (supabaseUrl == null || supabaseKey == null) {
-      throw Exception('Missing Supabase configuration');
-    }
-
     await getIt<SupabaseService>().initialize(
-      supabaseUrl: supabaseUrl,
-      supabaseKey: supabaseKey,
+      supabaseUrl: 'https://kizgmgaocdhnarvqtzvf.supabase.co',
+      supabaseKey:
+          'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtpemdtZ2FvY2RobmFydnF0enZmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzMzMjQ5NjksImV4cCI6MjA0ODkwMDk2OX0.LwosgMdM5ZcZAeVxn3b84lIeO4K6_-l4BsYF5pxxkJg',
     );
 
     await getIt<LocalStorageService>().init();
@@ -51,9 +44,7 @@ void main() async {
 
     runApp(const MyApp());
   } catch (e) {
-    print("Initialization error: $e");
-
-    rethrow;
+    print(e);
   }
 }
 
